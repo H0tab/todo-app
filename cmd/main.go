0d814d7f14906ff4a6852796be6aba9a955c5bec
H0tab <ksasha18@mail.ru>
2022-07-09 00:41:14 +0300

    added migrations and config files

--- cmd/main.go
+++ cmd/main.go
@@ -1,16 +1,31 @@
-package cmd
+package main
 
 import (
 	"github.com/H0tab/todo-app"
 	"github.com/H0tab/todo-app/pkg/handler"
+	"github.com/H0tab/todo-app/pkg/repository"
+	"github.com/H0tab/todo-app/pkg/service"
+	"github.com/spf13/viper"
 	"log"
 )
 
 func main() {
-	handlers := new(handler.Handler)
+	if err := initConfig(); err != nil {
+		log.Fatalf("error initializing configs: %s", err.Error())
+	}
+
+	repos := repository.NewRepository()
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
 
 	srv := new(todo.Server)
-	if err := srv.Run("8000", handlers.Initroutes()); err != nil {
+	if err := srv.Run(viper.GetString("port"), handlers.Initroutes()); err != nil {
 		log.Fatalf("error occured while running http server: %s", err.Error())
 	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
