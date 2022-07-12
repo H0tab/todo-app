2efdc2514d6f9896cd3d63b0fef63e7089b2b7b7
H0tab <ksasha18@mail.ru>
2022-07-12 21:35:03 +0300

    Parsing JWT tokens

--- pkg/service/auth.go
+++ pkg/service/auth.go
@@ -2,6 +2,7 @@ package service
 
 import (
 	"crypto/sha256"
+	"errors"
 	"fmt"
 	"github.com/H0tab/todo-app"
 	"github.com/H0tab/todo-app/pkg/repository"
@@ -49,6 +50,26 @@ func (s *AuthService) GenerateToken(username string, passsword string) (string, 
 	})
 
 	return token.SignedString([]byte(signingKey))
+}
+
+func (s *AuthService) ParseToken(accessToken string) (int, error) {
+	token, err := jwt.ParseWithClaims(accessToken, &tokenClaims{}, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, errors.New("invalid signing method")
+		}
+
+		return []byte(signingKey), nil
+	})
+	if err != nil {
+		return 0, err
+	}
+
+	claims, ok := token.Claims.(*tokenClaims)
+	if !ok {
+		return 0, errors.New("token claims are not of type *tokenClaims")
+	}
+
+	return claims.UserId, nil
 }
 
 func (s *AuthService) generatePasswordHash(password string) string {
